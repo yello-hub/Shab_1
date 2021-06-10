cfae40f3e820d1c7f5355711df5ba41e39efdd75
oongus <oognus@gmail.com>
2021-06-10 14:38:31 +0900

    미션1-2

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -45,7 +45,25 @@ class _MainListState extends State<MainListExample> {
       future: Stadium.fetchStadiums(),
       builder: (context, snapshot) {
       if (snapshot.hasData) {
+
+        // 이건 0번째(코딩에서 리스트는 0번부터 시작함) 리스트를 불러오는 코드입니다
         Stadium stadium = snapshot.data[0];
+        print(stadium.name);
+
+        // 이건 1번째 리스트를 불러오는 코드 입니다
+        Stadium stadium1 = snapshot.data[1];
+        print(stadium1.name);
+
+        // 이건 fetchStadiums() 로 불러온 리스트를 for문을 활용해 순차적으로 불러오는 코드 입니다
+        for (int i=0; i<snapshot.data.length; i++) {
+          Stadium stadium = snapshot.data[i];
+          print(stadium.name);
+        }
+
+        // 세가지를 참고해서
+        // ListView를 구성해보세요
+
+
         return Center(
           child: Text(stadium.name),
           //응답 받은 데이터로 UI 그리기
